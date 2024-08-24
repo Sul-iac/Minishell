9e7da472b6516f58423e8ade1dfb89aff650b5c1
Sul-iac <122944617+Sul-iac@users.noreply.github.com>
2024-08-24 19:05:07 +0200

    lexer v1
    commentaires ajoutes

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,11 +6,14 @@
 /*   By: qbarron <qbarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 15:19:38 by qbarron           #+#    #+#             */
-/*   Updated: 2024/08/24 18:25:43 by qbarron          ###   ########.fr       */
+/*   Updated: 2024/08/24 19:04:19 by qbarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+// infos: j'ai du mettre les fonctions standard parceque flemme de link le makefile de la libft et tout
+
 
 // creer les tokens
 t_token	*create_tokens(t_token_type *type, const char *value)
@@ -19,13 +22,13 @@ t_token	*create_tokens(t_token_type *type, const char *value)
 
 	token = malloc(sizeof(t_token));
 	if(token == NULL)
-		return(NULL);			// envoyer a la fonction d'erreur pour free.
-	token->type = type;
-	token->value = strdup(value);
+		return(NULL);											// envoyer a la fonction d'erreur pour free.
+	token->type = type;											// met le type de token correspondant dans la strcture
+	token->value = strdup(value);								// duplique 
 	return(token);
 }
 
-// verifie si l'input est une chaine de charactere
+// verifie si l'input est une commande ou une option
 const char	*tokenize_commands_options(const char *start, const char *end,
 							t_token **tokens, int *count_token)
 {
@@ -33,33 +36,35 @@ const char	*tokenize_commands_options(const char *start, const char *end,
 	char			*value;
 	t_token_type	*type;
 	
-	if(isalpha(*start) || *start == '-')
+	if(isalpha(*start) || *start == '-')								// si la fonction retourne 0, ou qu'on rencontre un tiret
 		{
-			end = start;
+			end = start;												// on initalise afin de trouver les options
 			while(isalnum(*end) || *end == '-' || *end == '/')
 				end++;
 			length = end - start;
-			value = strndup(start, length);
-			type = NULL;
-			if(*start == '-')
-				type = &token_types[1];				// options
-			else
-				type = &token_types[0];				// commandes
-			tokens[(*count_token)++] = create_tokens(type, value);
-			start = end;
+			value = strndup(start, length);								// malloc la commande ou l'option
+			type = NULL;												
+			if(*start == '-')											// s'il y a un tiret, c'est une option
+				type = &token_types[1];
+			else														// sinon c'est une commande
+				type = &token_types[0];
+			tokens[(*count_token)++] = create_tokens(type, value);		// on creer le token
+			start = end;												// on reinitialise pour continuer
 			free(value);
 		}
 		return(start);
 }
 
+
+// verifie si operateur complexe
 const char	*tokenize_complex_operators(const char *start, t_token **tokens, int *count_token)
 {
-		if(start[0] == '>' && start[1] == '>')
+		if(start[0] == '>' && start[1] == '>')									// si les deux premiers characteres sont complex
 		{
-			tokens[(*count_token)++] = create_tokens(&token_types[7], ">>");
+			tokens[(*count_token)++] = create_tokens(&token_types[7], ">>");	// on creer
 			start += 2;
 		}
-		else if(start[0] == '<' && start[1] == '<')
+		else if(start[0] == '<' && start[1] == '<')								// pareil
 		{
 			tokens[(*count_token)++] = create_tokens(&token_types[8], "<<");
 			start += 2;
@@ -70,9 +75,9 @@ const char	*tokenize_complex_operators(const char *start, t_token **tokens, int 
 // verifie si l'input est un operateur
 const char	*tokenize_operators(const char *start, t_token **tokens, int *count_token)
 {
-		if(*start == '|')
+		if(*start == '|')													// fais la meme chose que tokenize_complex_operator
 		{
-			tokens[(*count_token)++] =  create_tokens(&token_types[4], "|");
+			tokens[(*count_token)++] = create_tokens(&token_types[4], "|");			
 			start++;
 		}
 		else if(*start == '>')
@@ -103,18 +108,18 @@ int main()
 	end = NULL;
 	while(*start)
 	{
-		while(isspace(*start))														// on skippe le whitespaces
+		while(isspace(*start))															// on skippe les whitespaces s'il y en a
 			start++;
-		if(*start == '\0')															// verifie si il y a qqchs
+		if(*start == '\0')																// verifie si il y a qqchs
 			break ; 
-		start = tokenize_commands_options(start, end, tokens, &count_token);		// tokenise les commandes et options
-		start = tokenize_operators(start, tokens, &count_token);					// tokenise les operateurs communs
+		start = tokenize_commands_options(start, end, tokens, &count_token);			// tokenise les commandes et options
+		start = tokenize_operators(start, tokens, &count_token);						// tokenise les operateurs communs
 		if((start[0] == '>' && start[1] == '>') || (start[0] == '<' && start[1] == '<'))
-			start = tokenize_complex_operators(start, tokens, count_token);			// tokenise les operateurs complexes
+			start = tokenize_complex_operators(start, tokens, &count_token);			// tokenise les operateurs complexes
 		start++;
 	}
 	for (int i = 0; i < count_token; i++) {
-        printf("Token type: %s, value: %s\n", tokens[i]->type->name, tokens[i]->value);		// print (pour le test)
+        printf("Token type: %s, value: %s\n", tokens[i]->type->name, tokens[i]->value);	// print (pour le test)
         free(tokens[i]->value);
         free(tokens[i]);
     }
--- src/test_lexer.sh
+++ src/test_lexer.sh
@@ -2,12 +2,11 @@
 
 SRC_DIR="./lexer"
 INCLUDE_DIR="../includes"
-INCLUDE_LIBFT="./libft"
 TEST_EXEC="test_lexer"
 
 SRC_FILES="$SRC_DIR/lexer.c"
 
-gcc -I$INCLUDE_DIR -I$INCLUDE_LIBFT $SRC_FILES -o $TEST_EXEC
+gcc -I$INCLUDE_DIR $SRC_FILES -o $TEST_EXEC
 
 if [ $? -eq 0 ]; then
     echo "Compilation réussie."
