5d24a8d6465ace58b2e25b577588a66cddc9ca08
Suliac <122944617+Sul-iac@users.noreply.github.com>
2024-10-27 15:56:24 +0100

    v0.2 export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: qbarron <qbarron@student.42perpignan.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 23:09:03 by tgerardi          #+#    #+#             */
-/*   Updated: 2024/10/27 15:08:57 by qbarron          ###   ########.fr       */
+/*   Updated: 2024/10/27 15:34:16 by qbarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 // export seul display les variables d'env dans l'ordre alphabetique
 // export avec args set une nouvelle variable dans l'env
+
+static int check_errors(char **new_env)
+{
+	
+}
+
 void    ft_export(char *args, char **env)
 {
 	int	i;
@@ -21,7 +27,7 @@ void    ft_export(char *args, char **env)
     char **new_env;
 	char **new_var;
 	len = -1;
-	i = -1;	
+	i = -1;
 	while(env[++len]);
 	new_env = malloc(sizeof(char *) * (len + 2));
 	if(!new_env)
@@ -35,23 +41,29 @@ void    ft_export(char *args, char **env)
 	}
 	new_env[i] = NULL;
 	i = -1;
+	while(env[++i])
+		free(env[i]);
+	i = -1;
 	if(!args)
 	{
-		while(env[++i])
-			printf("declare -x %s\n", env[i]);
+		while(new_env[++i])
+			printf("declare -x %s\n", new_env[i]);
 	}
 	else
 	{
 		i = -1;
 		new_var = ft_split(args, ' ');
-		while(args[++i])
+		while(new_var[++i])
 		{
-			if(strchr(new_var[i], "=") == 0)
+			if(check_errors(new_var) == 0) // test=bonjour
 			{
 				
 			}
 			else
 				error();
 		}
+		i = -1;
+		while(new_env[++i])
+			printf("declare -x %s\n", new_env[i]);
 	}
 }
